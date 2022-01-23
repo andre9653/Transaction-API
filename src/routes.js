52a8026a9eae020faa2fc632c1e9bdca4a4617a7
andre9653 <andresena9653@gmail.com>
2022-01-23 17:52:28 -0300

    rota para criação da tabela de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const userController = require('./controllers/userController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.status(200).json({ teste: 'ok' }));
+routes.post('/users', userController.store);
 
 module.exports = routes;
