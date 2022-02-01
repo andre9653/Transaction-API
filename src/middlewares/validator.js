cd8df087c1458a2c8dac533b92cc122ad34fcbeb
andre9653 <andresena9653@gmail.com>
2022-01-31 22:05:21 -0300

    refatoraçao do validator

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,57 +1,14 @@
-const Validators = require('../services/validators/user.validator');
-const ValidatorAmount = require('../services/validators/depositValidator');
-const accountsValidator = require('../services/validators/accountsValidator');
+const ValidationError = require('../errors/ValidationError');
 
-const validRegister = async (req, res, next) => {
+const validator = (schema) => async (req, res, next) => {
   try {
-    const validated = await Validators.register.validateAsync(req.body);
-    req.body = validated;
+    req.body = await schema.validateAsync(req.body);
     return next();
   } catch (err) {
-    return res.status(401).json({ Error: err.message });
-  }
-};
-const validLogin = async (req, res, next) => {
-  try {
-    const validated = await Validators.login.validateAsync(req.body);
-    req.body = validated;
-    return next();
-  } catch (err) {
-    return res.status(401).json({ Error: err.message });
-  }
-};
-const validDeposit = async (req, res, next) => {
-  try {
-    const validated = await ValidatorAmount.deposit.validateAsync(req.body);
-    req.body = validated;
-    return next();
-  } catch (err) {
-    return res.status(401).json({ Error: err.message });
-  }
-};
-const validTransfer = async (req, res, next) => {
-  try {
-    const validated = await ValidatorAmount.transfer.validateAsync(req.body);
-    req.body = validated;
-    return next();
-  } catch (err) {
-    return res.status(401).json({ Error: err.message });
-  }
-};
-const validRegisterAmount = async (req, res, next) => {
-  try {
-    const validated = await accountsValidator.validateAsync(req.body);
-    req.body = validated;
-    return next();
-  } catch (err) {
-    return res.status(401).json({ Error: err.message });
+    return next(new ValidationError(err.message));
   }
 };
 
 module.exports = {
-  validRegister,
-  validLogin,
-  validDeposit,
-  validTransfer,
-  validRegisterAmount,
+  validator,
 };
