319fc7a940fdef24bc031d729bc25878818a3e3f
andre9653 <andresena9653@gmail.com>
2022-01-31 22:04:18 -0300

    refatoração da nomeclatura das rotas

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,22 @@ const express = require('express');
 const accountController = require('./controllers/accountController');
 const usersController = require('./controllers/userController');
 const Auth = require('./middlewares/Auth');
-const valid = require('./middlewares/validator');
+const usersValidations = require('./services/validators/userValidator');
+const accountsValidations = require('./services/validators/accountsValidator');
+const depositValidations = require('./services/validators/depositValidator');
+const { validator } = require('./middlewares/validator');
 
 const routes = express.Router();
 
-routes.get('/', usersController.index);
-routes.post('/register', valid.validRegister, usersController.store);
 // Rota de login quando bem sucedido retorna um token de autenticação.
-routes.post('/login', valid.validLogin, usersController.login);
-// Quando finalizado o cadastro, devera ser redirecionado para a rota de montante e inicial.
-routes.post('/register/:user_id/amount', valid.validRegisterAmount, accountController.store);
+routes.post('/login', validator(usersValidations.login), usersController.login);
 
-routes.post('/payment/', valid.validTransfer, Auth.validToken, accountController.payment);
-routes.put('/deposit', valid.validDeposit, accountController.deposit);
+routes.get('/users', usersController.index);
+routes.post('/users/register', validator(usersValidations.register), usersController.store);
+// Quando finalizado o cadastro, devera ser redirecionado para a rota de montante e inicial.
+routes.post('/accounts/register/:user_id', validator(accountsValidations.amount), accountController.store);
+
+routes.post('/accounts/payment', validator(depositValidations.transfer), Auth.validToken, accountController.payment);
+routes.put('/accounts/deposit', validator(depositValidations.deposit), accountController.deposit);
 
 module.exports = routes;
