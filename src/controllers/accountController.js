1c547e89c2bbea79034a4f481c104ee1a82c3fb9
andre9653 <andresena9653@gmail.com>
2022-01-31 22:05:00 -0300

    refatoração do controller de account para injetar dependencia e middleware de erros

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,20 +1,24 @@
 /* eslint-disable camelcase */
 const httpStatus = require('http-status');
+const database = require('../config/database');
 const accountServices = require('../services/accountServices');
 const depositServices = require('../services/depositServices');
 const { transactionsServices } = require('../services/transactionsServices');
 
 const accountController = () => {
+  const transactionService = transactionsServices(database);
+  const accountService = accountServices();
+
   const store = async (req, res) => {
     const { user_id } = req.params;
     const { startAmount } = req.body;
-    const result = await accountServices().store(user_id, startAmount);
+    const result = await accountService.store(user_id, startAmount);
     return res.status(httpStatus.CREATED).json(result);
   };
 
   const payment = async (req, res) => {
     const { id } = req.user;
-    const { status, message } = await transactionsServices().payment(id, req.body);
+    const { status, message } = await transactionService.payment(id, req.body);
     return res.status(status).json({ message });
   };
 
